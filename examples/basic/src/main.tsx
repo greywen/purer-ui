dded26d4d6977241f0754bdaec46b1ff57c42d6b
gray <gray.wen@outlook.com>
2025-05-16 10:44:32 +0800

    ci:install package error

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider, ThemeWatcher } from '@purer-ui/themes';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="system">
-      <ThemeWatcher />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
-); 
+);
